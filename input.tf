cf1b89b86e51441627322e63cce035be17efc32d
Craig Thacker <craigthackerx@gmail.com>
2022-05-30 22:54:31 +0100

    update

--- input.tf
+++ input.tf
@@ -1,26 +1,5 @@
 variable "standard_os" {
   default = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    "CentOS7"      = "OpenLogic,CentOS,7_8"
-    "CentOS8"      = "OpenLogic,CentOS,8_5"
-    "Debian10"     = "Debian,debian-10,10"
-    "Debian11"     = "Debian,debian-11,11"
-    "OracleLinux7" = "Oracle,Oracle-Linux,ol79"
-    "OracleLinux8" = "Oracle,Oracle-Linux,ol82"
-    "FlatcarFree"  = "kinvolk,flatcar-container-linux-free,stable"
-    "FlatcarPro"   = "kinvolk,flatcar_pro,stable"
-    "openSuse15"   = "SUSE,openSUSE-Leap,15-2"
-    "RHEL7"        = "RedHat,RHEL,7_9"
-    "RHEL8"        = "RedHat,RHEL,8_5"
-    "SLES12"       = "SUSE,SLES,12-sp4-gen2"
-    "Ubuntu16.04"  = "Canonical,UbuntuServer,16.04-LTS"
-    "Ubuntu18.04"  = "Canonical,UbuntuServer,18.04-LTS"
-    "Ubuntu20.04"  = "Canonical,0001-com-ubuntu-server-focal,20_04-lts"
-    "Ubuntu22.04"  = "Canonical,0001-com-ubuntu-server-jammy,22_04-lts"
-=======
-=======
->>>>>>> main
     "CentOS7"          = "OpenLogic,CentOS,7_8"
     "CentOS8"          = "OpenLogic,CentOS,8_5"
     "Debian10"         = "Debian,debian-10,10"
@@ -53,10 +32,6 @@ variable "standard_os" {
     "Ubuntu18.04Gen2"  = "Canonical,UbuntuServer,18_04-lts-gen2"
     "Ubuntu20.04Gen2"  = "Canonical,0001-com-ubuntu-server-focal,20_04-lts-gen2"
     "Ubuntu22.04Gen2"  = "Canonical,0001-com-ubuntu-server-jammy-daily,22_04-daily-lts-gen2"
-<<<<<<< HEAD
->>>>>>> 25f4351 (Update)
-=======
->>>>>>> main
   }
 }
 
